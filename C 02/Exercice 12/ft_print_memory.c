326a2e68e87c3b037220714a5e5ac975ace80a56
Thomas Choquet <thomas.publique@icloud.com>
2023-02-15 12:53:12 +0900

    Ex12 over

--- C 02/Exercice 12/ft_print_memory.c
+++ C 02/Exercice 12/ft_print_memory.c
@@ -6,15 +6,95 @@
 /*   By: tchoquet <tchoquet@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 10:10:27 by tchoquet          #+#    #+#             */
-/*   Updated: 2023/02/15 10:15:04 by tchoquet         ###   ########.fr       */
+/*   Updated: 2023/02/15 12:52:00 by tchoquet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include<unistd.h>
 
 void	*ft_print_memory(void *addr, unsigned int size);
+void	ft_print_line(void *addr, unsigned int size);
+char	*ft_dec_to_hex(unsigned long dec, int output_size);
+void	ft_print_ascii(void *addr, unsigned int size);
 
 void	*ft_print_memory(void *addr, unsigned int size)
 {
-	
+	unsigned int	i;
+
+	i = 0;
+	while (i < (size / 16))
+	{
+		ft_print_line(addr + (i * 16), 16);
+		i++;
+	}
+	if (size % 16 != 0)
+		ft_print_line(addr + (i * 16), size % 16);
+	return (addr);
+}
+
+void	ft_print_line(void *addr, unsigned int size)
+{
+	unsigned int	i;
+
+	write(1, ft_dec_to_hex((unsigned long)addr, 8), 16);
+	write(1, ": ", 2);
+	i = 0;
+	while (i < 16)
+	{
+		if (i < size)
+			write(1, ft_dec_to_hex(*((char *)addr + i), 1), 2);
+		else
+			write(1, "  ", 2);
+		i++;
+		if (i < size)
+			write(1, ft_dec_to_hex(*((char *)addr + i), 1), 2);
+		else
+			write(1, "  ", 2);
+		write(1, " ", 1);
+		i++;
+	}
+	ft_print_ascii(addr, size);
+	write(1, "\n", 1);
+}
+
+char	*ft_dec_to_hex(unsigned long dec, int output_size)
+{
+	static char	hex[17];
+	int			i;
+	int			remainder;
+
+	i = 0;
+	while (i < output_size * 2)
+	{
+		hex[i] = '0';
+		i++;
+	}
+	hex[output_size * 2] = '\0';
+	i = (output_size * 2) - 1;
+	while (dec > 0 && i >= 0)
+	{
+		remainder = dec % 16;
+		if (remainder < 10)
+			hex[i] = remainder + '0';
+		else
+			hex[i] = remainder - 10 + 'a';
+		dec /= 16;
+		i--;
+	}
+	return (hex);
+}
+
+void	ft_print_ascii(void *addr, unsigned int size)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (i < size)
+	{
+		if (((char *)addr)[i] < ' ' || ((char *)addr)[i] > '~')
+			write(1, ".", 1);
+		else
+			write(1, ((char *)addr) + i, 1);
+		i++;
+	}
 }
--- C 02/Exercice 12/main.c
+++ C 02/Exercice 12/main.c
@@ -6,7 +6,7 @@
 /*   By: tchoquet <tchoquet@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 09:58:43 by tchoquet          #+#    #+#             */
-/*   Updated: 2023/02/15 10:03:25 by tchoquet         ###   ########.fr       */
+/*   Updated: 2023/02/15 12:41:45 by tchoquet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,46 @@
 #include <stdlib.h>
 #include <string.h>
 
+void	*ft_print_memory(void *addr, unsigned int size);
+/*
 int main()
 {
 	void* startPtr = malloc(42);
-	memset(startPtr, 0xAA, 25);
+	memset(startPtr, 0xAA, 20);
+	memset(startPtr + 20, 0xBB, 20);
+	memset(startPtr + 40, 0xCC, 2);
 
-	printf("%d Bytes from %p\n",25, startPtr);
+	printf("%d Bytes from %p\n",42, startPtr);
+	ft_print_memory(startPtr, 42);
 	return 0;
+}
+*/
+
+int main()
+{
+    char *str1 = "Hello World!";
+    printf("=== Test 1 ===\n");
+    ft_print_memory(str1, 13);
+
+    char *str2 = "This string has some non-printable characters: \x00\x01\x1F\x7F";
+    printf("\n=== Test 2 ===\n");
+    ft_print_memory(str2, 51);
+
+    char str3[8];
+    for (int i = 0; i < 8; i++)
+    {
+        str3[i] = (char)(i * 16 + i);
+    }
+    printf("\n=== Test 3 ===\n");
+    ft_print_memory(str3, 6);
+
+    int num = 2147483647;
+    printf("\n=== Test 4 ===\n");
+    ft_print_memory(&num, sizeof(num));
+
+    char *ptr = NULL;
+    printf("\n=== Test 5 ===\n");
+    ft_print_memory(ptr, 0);
+
+    return 0;
 }

4e9ab51f6490c663c40273cd9bdbec0262e7e407
Thomas Choquet <thomas.publique@icloud.com>
2023-02-15 22:30:41 +0900

    Ex02

---
+++ C 03/Exercice 02/ft_strcat.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tchoquet <tchoquet@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/15 20:02:48 by tchoquet          #+#    #+#             */
+/*   Updated: 2023/02/15 22:29:37 by tchoquet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strcat(char *dest, char *src);
+
+char	*ft_strcat(char *dest, char *src)
+{
+	unsigned long long	i;
+	unsigned long long	y;
+
+	i = 0;
+	while (dest[i] != '\0')
+		i++;
+	y = 0;
+	while (src[y] != '\0')
+	{
+		dest[i] = src[y];
+		i++;
+		y++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
---
+++ C 03/Exercice 02/main.c
@@ -0,0 +1,102 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tchoquet <tchoquet@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/15 20:08:47 by tchoquet          #+#    #+#             */
+/*   Updated: 2023/02/15 22:29:07 by tchoquet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+char	*ft_strcat(char *dest, char *src);
+
+char* decToHex(uint8_t decimal);
+char* getMemHex(void* startPtr, int len);
+
+int main()
+{
+	char* s1 = malloc(5);
+	memset(s1, 0xFF, 5);
+	char* s2 = malloc(3);
+	memset(s2, 0xFF, 5);
+
+	memset(s1, 0xAA, 2);
+	memset(s1 + 2, 0x0, 1);
+
+	memset(s2, 0xBB, 2);
+	memset(s2 + 2, 0x0, 1);
+
+	printf("'%s' + '%s' = '%s'\n", getMemHex(s1, 5), 
+			getMemHex(s2, 3), 
+			getMemHex(ft_strcat(s1, s2), 5));
+
+	memset(s1, 0xFF, 5);
+	memset(s1, 0xAA, 2);
+	memset(s1 + 2, 0x0, 1);
+	
+	memset(s2, 0x0, 5);
+
+	printf("'%s' + '%s' = '%s'\n", getMemHex(s1, 5), 
+			getMemHex(s2, 3), 
+			getMemHex(ft_strcat(s1, s2), 5));
+
+	memset(s1, 0x0, 5);
+	
+	memset(s2, 0xFF, 5);
+	memset(s2, 0xBB, 2);
+	memset(s2 + 2, 0x0, 1);
+
+	printf("'%s' + '%s' = '%s'\n", getMemHex(s1, 5), 
+			getMemHex(s2, 3), 
+			getMemHex(ft_strcat(s1, s2), 5));
+
+	return 0;
+}
+
+char* getMemHex(void* startPtr, int len)
+{
+	char* mem = malloc(sizeof(char) * (len * 3 - 1));
+	if (mem == NULL) exit(1);
+	
+	char* hex = decToHex(*((u_int8_t*)startPtr));
+	memcpy(mem, hex, 2);
+	free(hex);
+	mem[2] = ' ';
+	for (int i = 1, y = 3; i < len; i++, y+=3)
+	{
+		hex = decToHex(*(((u_int8_t*)startPtr)+i));
+		memcpy(mem + y, hex, 2);
+		free(hex);
+		mem[y+2]=' ';
+	}
+	*(mem + (len * 3 - 1)) = '\0';
+	return mem;
+}
+
+char* decToHex(uint8_t decimal) 
+{
+	char* hexa = malloc(sizeof(char) * 3);
+
+	hexa[0] = decimal / 16;
+	hexa[1] = decimal % 16;
+
+	for (int i = 0; i <= 1; i++)
+	{
+		if (hexa[i] < 10) 
+		{
+			hexa[i] += 48;
+		}
+		else 
+		{
+			hexa[i] += 55;
+		}
+	}
+	hexa[2] = '\0';
+	return hexa;
+}

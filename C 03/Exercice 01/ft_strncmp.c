bca0086e0c21c60db77efc817c5ec053812c7452
Thomas Choquet <thomas.publique@icloud.com>
2023-02-15 20:01:11 +0900

    Ex01 over

---
+++ C 03/Exercice 01/ft_strncmp.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tchoquet <tchoquet@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/15 19:13:22 by tchoquet          #+#    #+#             */
+/*   Updated: 2023/02/15 19:54:04 by tchoquet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strncmp(char *s1, char *s2, unsigned int n);
+
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (s1[i] && s2[i] && (i < n))
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (s1[i] - s2[i]);
+} 
--- C 03/Exercice 01/main.c
+++ C 03/Exercice 01/main.c
@@ -6,28 +6,33 @@
 /*   By: tchoquet <tchoquet@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 19:03:13 by tchoquet          #+#    #+#             */
-/*   Updated: 2023/02/15 19:06:54 by tchoquet         ###   ########.fr       */
+/*   Updated: 2023/02/15 19:54:25 by tchoquet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include<stdio.h>
+#include<string.h>
 
-int ft_strncmp(char *s1, char *s2, unsigned int n)
-{
-	unsigned int i;
+int	ft_strncmp(char *s1, char *s2, unsigned int n);
 
-	i = 0;
-	while (i < n)
-	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
-		i++;
-	}
-	return (0);
-} 
 
 int main()
 {
-	printf("Hello World!");
-	return (0);
+    char *str1 = "Hello, world!";
+    char *str2 = "Hello, world!";
+    printf("'%s' and '%s' : \nft_strncmp 13 -> %d | strncmp 13 -> %d\n\n", str1, str2, ft_strncmp(str1, str2, 13), strncmp(str1, str2, 13));
+
+	str1 = "Hello, world!";
+    str2 = "Hello, everyone!";
+	printf("'%s' and '%s' : \nft_strncmp 5 -> %d | strncmp 5 -> %d\n\n", str1, str2, ft_strncmp(str1, str2, 5), strncmp(str1, str2, 5));
+
+	str1 = "";
+    str2 = "";
+	printf("'%s' and '%s' : \nft_strncmp 5 -> %d | strncmp 5 -> %d\n\n", str1, str2, ft_strncmp(str1, str2, 5), strncmp(str1, str2, 5));
+
+	str1 = "Hello, world!";
+    str2 = "Hello,\x15\x11\xffveryone!";
+	printf("'%s' and '%s' : \nft_strncmp 17 -> %d | strncmp 17 -> %d\n\n", str1, str2, ft_strncmp(str1, str2, 17), strncmp(str1, str2, 17));
+
+    return 0;
 }

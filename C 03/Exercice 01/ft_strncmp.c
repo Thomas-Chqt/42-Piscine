7f3cc2f37fe95b86cd7ca48057c926e1c0d9d272
Thomas Choquet <thomas.publique@icloud.com>
2023-02-16 16:10:15 +0900

    fix Ex01

--- C 03/Exercice 01/ft_strncmp.c
+++ C 03/Exercice 01/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: tchoquet <tchoquet@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 19:13:22 by tchoquet          #+#    #+#             */
-/*   Updated: 2023/02/16 13:26:33 by tchoquet         ###   ########.fr       */
+/*   Updated: 2023/02/16 16:08:09 by tchoquet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,17 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
 	unsigned int	i;
 
+	if (s1[0] == '\0')
+		return (s1[0] - s2[0]);
 	i = 0;
-	while (s1[i] && s2[i] && (i < n))
+	while (i < n)
 	{
+		if (s1[i] == '\0')
+			return (0);
 		if (s1[i] != s2[i])
 			return (s1[i] - s2[i]);
 		i++;
 	}
-	return (s1[i] - s2[i]);
+	return (0);
+	
 }
--- C 03/Exercice 01/main.c
+++ C 03/Exercice 01/main.c
@@ -6,7 +6,7 @@
 /*   By: tchoquet <tchoquet@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 19:03:13 by tchoquet          #+#    #+#             */
-/*   Updated: 2023/02/15 19:54:25 by tchoquet         ###   ########.fr       */
+/*   Updated: 2023/02/16 16:09:42 by tchoquet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,26 @@ int main()
 	str1 = "Hello, world!";
     str2 = "Hello,\x15\x11\xffveryone!";
 	printf("'%s' and '%s' : \nft_strncmp 17 -> %d | strncmp 17 -> %d\n\n", str1, str2, ft_strncmp(str1, str2, 17), strncmp(str1, str2, 17));
+
+	str1 = "Hello, world!";
+    str2 = "Hello, everyone!";
+	printf("'%s' and '%s' : \nft_strncmp 0 -> %d | strncmp 0 -> %d\n\n", str1, str2, ft_strncmp(str1, str2, 0), strncmp(str1, str2, 0));
+
+	str1 = "hello world";
+	str2 = "hello";
+	printf("'%s' and '%s' : \nft_strncmp 5 -> %d | strncmp 5 -> %d\n\n", str1, str2, ft_strncmp(str1, str2, 5), strncmp(str1, str2, 5));
+
+	str1 = "";
+	str2 = "hello world";
+	printf("'%s' and '%s' : \nft_strncmp 5 -> %d | strncmp 5 -> %d\n\n", str1, str2, ft_strncmp(str1, str2, 5), strncmp(str1, str2, 5));
+
+	str1 = "hello world";
+	str2 = "";
+	printf("'%s' and '%s' : \nft_strncmp 5 -> %d | strncmp 5 -> %d\n\n", str1, str2, ft_strncmp(str1, str2, 5), strncmp(str1, str2, 5));
+
+	str1 = "hello world";
+	str2 = "";
+	printf("'%s' and '%s' : \nft_strncmp 0 -> %d | strncmp 0 -> %d\n\n", str1, str2, ft_strncmp(str1, str2, 0), strncmp(str1, str2, 0));
 
     return 0;
 }

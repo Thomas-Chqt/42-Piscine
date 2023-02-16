017e6c4c47d566cf549474662e16f6fab03a03cd
Thomas <semoir.dense-0h@icloud.com>
2023-02-16 10:28:05 +0900

    Exo4 over

---
+++ C 03/Exercice 04/ft_strstr.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tchoquet <tchoquet@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/16 10:22:28 by tchoquet          #+#    #+#             */
+/*   Updated: 2023/02/16 10:27:52 by tchoquet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strstr(char *str, char *to_find);
+
+char	*ft_strstr(char *str, char *to_find)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (str[i] != '\0')
+	{
+		while (str[i + j] == to_find[j])
+		{
+			if (to_find[j + 1] == '\0')
+				return (&str[i]);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
--- C 03/Exercice 04/main.c
+++ C 03/Exercice 04/main.c
@@ -6,21 +6,33 @@
 /*   By: tchoquet <tchoquet@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 23:39:12 by tchoquet          #+#    #+#             */
-/*   Updated: 2023/02/15 23:46:23 by tchoquet         ###   ########.fr       */
+/*   Updated: 2023/02/16 10:27:19 by tchoquet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include<stdio.h>
 #include<string.h>
 
+char	*ft_strstr(char *str, char *to_find);
+
 int main () {
-   char *big = "TutorialsPointTutorials";
-   char *little = "Point";
-   char *ret;
+   char *big = "ABCD";
+   char *little = "CD";
 
-   ret = strstr(big, little);
+   printf("Serch %s in %s -> return %s\n", little, big, ft_strstr(big, little));
 
-   printf("The substring is: %s\n", ret);
+   little = "AB";
+
+   printf("Serch %s in %s -> return %s\n", little, big, ft_strstr(big, little));
+
+	little = "DE";
+
+   printf("Serch %s in %s -> return %s\n", little, big, ft_strstr(big, little));
+
+   big = "";
+   little = "AB";
+
+   printf("Serch %s in %s -> return %s\n", little, big, ft_strstr(big, little));
    
    return(0);
 }

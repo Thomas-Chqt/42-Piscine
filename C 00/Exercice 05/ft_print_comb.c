694bf36008512bf9150289b1cf4d4eb8a98aaa82
Thomas Choquet <thomas.publique@icloud.com>
2023-02-17 19:38:09 +0900

    better ex05

--- C 00/Exercice 05/ft_print_comb.c
+++ C 00/Exercice 05/ft_print_comb.c
@@ -3,73 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   ft_print_comb.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tchoquet <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: tchoquet <tchoquet@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/11 10:01:46 by tchoquet          #+#    #+#             */
-/*   Updated: 2023/02/11 10:17:45 by tchoquet         ###   ########.fr       */
+/*   Updated: 2023/02/17 19:37:59 by tchoquet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include<unistd.h>
 
-#define START_MODULO 100
-#define NBR_SIZE 3
-
-void	ft_print_num(int nbr);
-int		need_print(int nbr);
-
 void	ft_print_comb(void)
 {
-	int	i;
+	char	comb[3];
 
-	i = 0;
-	while (i <= 999)
+	comb[0] = '0';
+	while (comb[0] <= '7')
 	{
-		if (need_print(i) == 1)
+		comb[1] = comb[0] + 1;
+		while (comb[1] <= '8')
 		{
-			ft_print_num(i);
-			if (i != 789)
-				write(1, ", ", 2);
+			comb[2] = comb[1] + 1;
+			while (comb[2] <= '9')
+			{
+				write(1, comb, 3);
+				if (comb[0] != '7' || comb[1] != '8' || comb[2] != '9')
+					write(1, ", ", 2);
+				comb[2]++;
+			}
+			comb[1]++;
 		}
-		i++;
+		comb[0]++;
 	}
-}
-
-void	ft_print_num(int nbr)
-{
-	int		modulo;
-	int		rest;
-	char	new_char;
-
-	modulo = START_MODULO;
-	rest = nbr;
-	while (modulo >= 1)
-	{
-		new_char = '0' + (rest / modulo);
-		write(1, &new_char, 1);
-		rest %= modulo;
-		modulo /= 10;
-	}
-}
-
-int	need_print(int nbr)
-{
-	int	tab[NBR_SIZE];
-	int	modulo;
-	int	rest;
-	int	nbr_digits;
-
-	modulo = START_MODULO;
-	rest = nbr;
-	nbr_digits = 0;
-	while (modulo >= 1)
-	{
-		tab[nbr_digits] = rest / modulo;
-		rest %= modulo;
-		modulo /= 10;
-		if ((nbr_digits > 0) && (tab[nbr_digits] <= tab[nbr_digits - 1]))
-			return (0);
-		nbr_digits++;
-	}
-	return (1);
 }

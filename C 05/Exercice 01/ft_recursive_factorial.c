5568c3b78bb7c539fa60846c07b1e7bd28d8bac7
Thomas Choquet <thomas.publique@icloud.com>
2023-02-21 20:44:44 +0900

    exo01

---
+++ C 05/Exercice 01/ft_recursive_factorial.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tchoquet <tchoquet@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/21 20:30:31 by tchoquet          #+#    #+#             */
+/*   Updated: 2023/02/21 20:44:34 by tchoquet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_factorial(int nb)
+{
+	if (nb < 0)
+		return (0);
+	if (nb == 0)
+		return (1);
+	else
+		return (nb * ft_recursive_factorial(nb - 1));
+}
---
+++ C 05/Exercice 01/main.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tchoquet <tchoquet@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/21 20:30:21 by tchoquet          #+#    #+#             */
+/*   Updated: 2023/02/21 20:41:01 by tchoquet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_recursive_factorial(int nb);
+
+int	main()
+{
+	int testValues[] = {1, 2, 3, 4, 5, -10, 0 };
+
+	for (int i = 0; i < 7; i++)
+	{
+		printf("%d! = %d\n", testValues[i], ft_recursive_factorial(testValues[i]));
+	}
+	return 0;
+}

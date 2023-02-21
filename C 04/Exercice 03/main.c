e28387d64280119c20a08acfecb3508ca573b00f
Thomas Choquet <thomas.publique@icloud.com>
2023-02-21 10:34:38 +0900

    change main ex3

--- C 04/Exercice 03/main.c
+++ C 04/Exercice 03/main.c
@@ -6,7 +6,7 @@
 /*   By: tchoquet <tchoquet@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 22:05:06 by tchoquet          #+#    #+#             */
-/*   Updated: 2023/02/20 23:36:49 by tchoquet         ###   ########.fr       */
+/*   Updated: 2023/02/21 10:33:23 by tchoquet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ int main()
 		"1234",
 	};
 
-	for (int i = 0; i < 6; i++)
+	for (int i = 0; i < 8; i++)
 	{
 		printf("Test with : '%s'\n", testStr[i]);
 		printf("atoi : %d\n\n", ft_atoi(testStr[i]));

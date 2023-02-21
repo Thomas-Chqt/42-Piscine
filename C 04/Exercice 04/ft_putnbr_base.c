fed9179fa73eaa3b3a24714708405590f6d16be4
Thomas Choquet <thomas.publique@icloud.com>
2023-02-21 14:03:58 +0900

    ex04

---
+++ C 04/Exercice 04/ft_putnbr_base.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_base.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tchoquet <tchoquet@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/21 11:08:55 by tchoquet          #+#    #+#             */
+/*   Updated: 2023/02/21 12:44:10 by tchoquet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <limits.h>
+
+typedef unsigned char	t_uint8;
+
+void	ft_putabnbr_base(unsigned int nbr, char *base);
+int		ft_check_base(char *base);
+int		ft_strlen(char *str);
+
+void	ft_putnbr_base(int nbr, char *base)
+{
+	long	l_nbr;
+
+	l_nbr = (long)nbr;
+	if (ft_check_base(base) != 0)
+		return ;
+	if (l_nbr < 0)
+	{
+		write(1, "-", 1);
+		l_nbr *= -1;
+	}
+	ft_putabnbr_base((unsigned int)l_nbr, base);
+}
+
+void	ft_putabnbr_base(unsigned int nbr, char *base)
+{
+	if ((nbr / ft_strlen(base)) > 0)
+	{
+		ft_putabnbr_base(nbr / ft_strlen(base), base);
+	}
+	write(1, &base[nbr % ft_strlen(base)], 1);
+}
+
+int	ft_check_base(char *base)
+{
+	int	i;
+	int	y;
+
+	if (base[0] == '\0' || base[1] == '\0')
+		return (1);
+	i = 0;
+	while (base[i])
+	{
+		if (base[i] == '+' || base[i] == '-')
+			return (2);
+		y = i + 1;
+		while (base[y])
+		{
+			if (base[i] == base[y])
+				return (3);
+			y++;
+		}
+		i++;
+	}
+	return (0);
+}
+
+int	ft_strlen(char *str)
+{
+	int	len;
+
+	len = 0;
+	while (str[len])
+		len++;
+	return (len);
+}
---
+++ C 04/Exercice 04/main.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tchoquet <tchoquet@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/21 11:09:14 by tchoquet          #+#    #+#             */
+/*   Updated: 2023/02/21 12:46:20 by tchoquet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <limits.h>
+
+typedef struct s_Test Test;
+struct s_Test
+{
+	int decNbr;
+	char *base;
+};
+
+
+void	ft_putnbr_base(int nbr, char *base);
+
+int main()
+{
+	Test tests[] = {
+		{170, "0123456789ABCDEF"},
+		{170, ""},
+		{170, "012345678+9ABCDEF"},
+		{170, "012345678-9ABCDEF"},
+		{170, "0123456789AACDEF"},
+		{170, "01"},
+		{170, "poneyvif"},
+		{-170, "0123456789ABCDEF"},
+		{-170, "01"},
+		{-170, "poneyvif"},
+		{170, "0123456789ABCDEF"},
+		{170, "01"},
+		{INT_MAX, "0123456789ABCDEF"},
+		{INT_MIN, "0123456789ABCDEF"},
+		{INT_MIN, "01"},
+	};
+
+	for (int i = 0; i < 15; i++)
+	{
+		printf("Test with %d in base %s :\n", tests[i].decNbr, tests[i].base);
+		ft_putnbr_base(tests[i].decNbr, tests[i].base);
+		printf("\n\n");
+	}
+	return 0;
+}

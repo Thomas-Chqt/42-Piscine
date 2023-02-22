cd494bef036e76095228716ce0ffa61b41a252c3
Thomas Choquet <thomas.publique@icloud.com>
2023-02-22 10:34:08 +0900

    imac

---
+++ C 05/Exercice 08/main.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tchoquet <tchoquet@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/21 23:28:35 by tchoquet          #+#    #+#             */
+/*   Updated: 2023/02/21 23:31:23 by tchoquet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_ten_queens_puzzle(void);
+
+int main()
+{
+	printf("%d Solutions found\n", ft_ten_queens_puzzle());
+	return 0;
+}
+

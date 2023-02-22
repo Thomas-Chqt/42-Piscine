e36e20394667cc0aeb571ab7a6325b1555161d85
Thomas Choquet <thomas.publique@icloud.com>
2023-02-22 22:09:37 +0900

    ex00

--- C 07/Exercice 00/ft_strdup.c
+++ C 07/Exercice 00/ft_strdup.c
@@ -6,14 +6,41 @@
 /*   By: tchoquet <tchoquet@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/22 21:30:37 by tchoquet          #+#    #+#             */
-/*   Updated: 2023/02/22 21:33:42 by tchoquet         ###   ########.fr       */
+/*   Updated: 2023/02/22 22:09:28 by tchoquet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <errno.h>
+#include <stdlib.h>
+
+long long	ft_strlen(char *str);
 
 char	*ft_strdup(char *src)
 {
+	long long	len;
+	char		*cp;
 
-	errno = ENOMEM;
+	len = ft_strlen(src);
+	cp = malloc(sizeof(char) * (len + 1));
+	if (cp == NULL)
+	{
+		errno = ENOMEM;
+		return (NULL);
+	}
+	while (len >= 0)
+	{
+		cp[len] = src[len];
+		len--;
+	}
+	return (cp);
+}
+
+long long	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
 }
--- C 07/Exercice 00/main.c
+++ C 07/Exercice 00/main.c
@@ -6,16 +6,31 @@
 /*   By: tchoquet <tchoquet@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/22 20:57:43 by tchoquet          #+#    #+#             */
-/*   Updated: 2023/02/22 21:34:42 by tchoquet         ###   ########.fr       */
+/*   Updated: 2023/02/22 22:03:22 by tchoquet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 char	*ft_strdup(char *src);
 
 int main()
 {
-	char* cp = strdup("abc");
+	char *testValues[] = {
+		"abc",
+		"Hello World!",
+	};
+
+	char* cp = NULL;
+	for (int i = 0; i < 2; i++)
+	{
+		cp = ft_strdup(testValues[i]);
+		printf("Copy of %s : \n%s\n", testValues[i], cp);
+		free(cp);
+	}
+	
+
 	return 0;
 }

080830c18bb22131fa74cf246afb380faa271d77
Thomas Choquet <thomas.publique@icloud.com>
2023-02-22 11:54:25 +0900

    norme ex08

--- C 05/Exercice 08/ft_ten_queens_puzzle.c
+++ C 05/Exercice 08/ft_ten_queens_puzzle.c
@@ -6,24 +6,25 @@
 /*   By: tchoquet <tchoquet@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 23:27:54 by tchoquet          #+#    #+#             */
-/*   Updated: 2023/02/22 11:40:42 by tchoquet         ###   ########.fr       */
+/*   Updated: 2023/02/22 11:54:14 by tchoquet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include<unistd.h>
 
-int ft_is_safe(char *grid, int new_q_index, char new_q_pos);
-int ft_recurse(char *grid, int index);
+int	ft_is_safe(char *grid, int new_q_index, char new_q_pos);
+int	ft_recurse(char *grid, int index);
 
 int	ft_ten_queens_puzzle(void)
 {
-	char grid[10];
+	char	grid[10];
+
 	return (ft_recurse(grid, 0));
 }
 
-int ft_is_safe(char *grid, int new_q_index, char new_q_pos)
+int	ft_is_safe(char *grid, int new_q_index, char new_q_pos)
 {
-	int i;
+	int	i;
 
 	i = new_q_index - 1;
 	while (i >= 0)
@@ -39,9 +40,9 @@ int ft_is_safe(char *grid, int new_q_index, char new_q_pos)
 	return (1);
 }
 
-int ft_recurse(char *grid, int index)
+int	ft_recurse(char *grid, int index)
 {
-	char	i;
+	char		i;
 	static int	solution_count = 0;
 
 	if (index >= 10)
@@ -62,4 +63,3 @@ int ft_recurse(char *grid, int index)
 	}
 	return (solution_count);
 }
-

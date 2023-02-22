7ecc57dc5cc3578448d2d45d05b52670ab79f50a
Thomas Choquet <thomas.publique@icloud.com>
2023-02-22 16:29:43 +0900

    fix ex05

--- C 05/Exercice 05/ft_sqrt.c
+++ C 05/Exercice 05/ft_sqrt.c
@@ -6,21 +6,23 @@
 /*   By: tchoquet <tchoquet@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 21:43:45 by tchoquet          #+#    #+#             */
-/*   Updated: 2023/02/21 21:50:57 by tchoquet         ###   ########.fr       */
+/*   Updated: 2023/02/22 16:26:05 by tchoquet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_sqrt(int nb)
 {
-	int	i;
+	unsigned long	i;
+	unsigned long 	long_nb;
 
 	if (nb <= 0)
 		return (0);
 	i = 1;
-	while ((i * i) <= nb)
+	long_nb = (unsigned long)nb;
+	while ((i * i) <= long_nb)
 	{
-		if ((i * i) == nb)
-			return (i);
+		if ((i * i) == long_nb)
+			return ((int)i);
 		i++;
 	}
 	return (0);
--- C 05/Exercice 05/main.c
+++ C 05/Exercice 05/main.c
@@ -6,19 +6,20 @@
 /*   By: tchoquet <tchoquet@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 21:48:07 by tchoquet          #+#    #+#             */
-/*   Updated: 2023/02/22 12:06:47 by tchoquet         ###   ########.fr       */
+/*   Updated: 2023/02/22 16:26:07 by tchoquet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include<stdio.h>
+#include<limits.h>
 
 int	ft_sqrt(int nb);
 
 int main()
 {
-	int testValue[] = { 9, 25, 81, -3, -9, 0, 3, 6, 72};
+	int testValue[] = { 9, 25, 81, -3, -9, 0, 3, 6, INT_MAX};
 
-	for (int i = 0; i < 8; i++)
+	for (int i = 0; i < 9; i++)
 	{
 		printf("sqrt(%d) = %d\n", testValue[i], ft_sqrt(testValue[i]));
 	}

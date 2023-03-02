30813862c15364face7bbf1dd642d2d5f3349a3f
Thomas Choquet <thomas.publique@icloud.com>
2023-03-02 22:58:57 +0900

    norme 05

--- C 11/Exercice 05/ft_do_op.c
+++ C 11/Exercice 05/ft_do_op.c
@@ -6,7 +6,7 @@
 /*   By: tchoquet <tchoquet@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 20:28:09 by tchoquet          #+#    #+#             */
-/*   Updated: 2023/03/02 22:16:58 by tchoquet         ###   ########.fr       */
+/*   Updated: 2023/03/02 22:58:45 by tchoquet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,5 @@ int	ft_do_op(int a, char op, int b)
 	op_array['/'] = &ft_divide;
 	op_array['*'] = &ft_multiply;
 	op_array['%'] = &ft_modulo;
-
 	return (op_array[(int)op](a, b));
 }
